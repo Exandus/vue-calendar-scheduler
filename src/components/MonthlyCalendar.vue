4520af4d595205b9997404e6b8c24010883d1cc3
Shaiket Das <shaiket.reads@gmail.com>
2019-02-16 20:25:26 -0800

    more boilerplate code

---
+++ src/components/MonthlyCalendar.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="hello">
+    {{ msg }}
+  </div>
+</template>
+
+<script>
+export default {
+  name: "MonthlyCalendar",
+  props: {
+    msg: String
+  }
+};
+</script>
+
+<style scoped lang="scss"></style>
